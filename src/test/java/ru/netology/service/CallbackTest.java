01e52dd862cccfe261ba940f797c2e3d6a18e620
Ярослав Черников <yaroslavcher@gmail.com>
2022-08-09 23:57:31 +0300

    testOK

--- src/test/java/ru/netology/service/CallbackTest.java
+++ src/test/java/ru/netology/service/CallbackTest.java
@@ -21,11 +21,11 @@ public class CallbackTest {
 
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
+/*        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        options.addArguments("--headless");*/
+        driver = new ChromeDriver();
     }
 
     @AfterEach
